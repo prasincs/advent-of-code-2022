f8d4b8aa54b427eda88955717bc0606fe573d3a5
Prasanna Gautam <prasannagautam@gmail.com>
2022-12-11 12:37:54 -0800

    day 11 fixes

--- src/day11.rs
+++ src/day11.rs
@@ -1,9 +1,10 @@
+use itertools::Itertools;
 use regex::Regex;
 use std::collections::HashMap;
 use std::hash::Hash;
 use std::vec;
 
-#[derive(Default, Debug)]
+#[derive(Default, Clone, Debug, PartialEq,PartialOrd, Eq)]
 struct Monkey {
     Name: String,
     ID: u32,
@@ -12,6 +13,13 @@ struct Monkey {
     TestDivisibleby: u32,
     TrueCase: u32,
     FalseCase: u32,
+}
+
+
+impl Ord for Monkey{
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        (self.ID).cmp(&other.ID)
+    }
 }
 
 pub fn run() {
@@ -94,41 +102,62 @@ pub fn run() {
             monkey = Default::default();
         }
     }
-    let mut moves_map: HashMap<u32, Vec<(u32, u32)>> = HashMap::new();
-    for (id, monkey) in monkeys.iter() {
-        let mut moves: Vec<(u32, u32)> = vec![];
-        println!("{:?}", monkey);
-        for worry_item in monkey.ItemWorries.iter() {
-            let new_worry_value = match monkey.Operation.0.as_str() {
+
+    run_round(0, &mut monkeys);
+    run_round(1, &mut monkeys);
+    // println!("{:?}", moves_map);
+    // for (id, moves) in moves_map {
+    //     if let Some(monkey) = monkeys.get_mut(&id) {
+    //         let to_remove: Vec<u32> = moves.iter().map(|x| x.0).collect();
+    //         let mut current_worries = monkey.ItemWorries.clone();
+    //         println!("[{}]{:?}, to_remove: {:?}", id, current_worries, to_remove);
+    //         current_worries.retain(|x| !to_remove.contains(x));
+    //         println!("[{}]{:?}", id, current_worries);
+    //     };
+
+    // }
+}
+
+fn run_round(round: u32, monkeys: &mut HashMap<u32, Monkey>) {
+    let mut new_worry_items: Vec<(u32,u32)> = vec![];
+    // we are going to mutate the original monkeys, so use a clone
+    for (id, monkey) in monkeys.clone().iter().sorted() {
+        let mut cur_item_worries = monkey.ItemWorries.clone();
+        cur_item_worries.extend(new_worry_items.iter().filter(|(move_id,_)| move_id == id ).map(|(_, worry_id)| worry_id));
+        for worry_item in cur_item_worries{
+            let current_worry_value = match monkey.Operation.0.as_str() {
                 "*" => monkey.Operation.1 * worry_item,
                 "+" => monkey.Operation.1 + worry_item,
                 "pow" => worry_item * worry_item,
                 &_ => {
-                    println!("{:?}", monkey.Operation);
-                    *worry_item
+                    // println!("{:?}", monkey.Operation);
+                    panic!("Invalid operation {:?}", monkey.Operation);
                 }
             };
+            let new_worry_value: u32 = current_worry_value / 3; 
             let remainder = new_worry_value % monkey.TestDivisibleby;
-            println!("worry={}, divisible by={}, remainder={}", new_worry_value, monkey.TestDivisibleby, remainder);
+            println!(
+                "monkey={},worry={}, divisible by={}, new_worry={}, remainder={}",
+                monkey.ID,
+                current_worry_value, monkey.TestDivisibleby, new_worry_value, remainder
+            );
             if remainder == 0 {
                 println!("move {} to monkey {}", worry_item, monkey.TrueCase);
-                moves.push((*worry_item, monkey.TrueCase))
+                let moved_monkey = monkeys.get_mut(&monkey.TrueCase).unwrap();
+                moved_monkey.ItemWorries.push(new_worry_value);
+                new_worry_items.push((monkey.TrueCase, new_worry_value));
             } else {
                 println!("move {} to monkey {}", worry_item, monkey.FalseCase);
-                moves.push((*worry_item, monkey.FalseCase))
+                let moved_monkey = monkeys.get_mut(&monkey.FalseCase).unwrap();
+                moved_monkey.ItemWorries.push(new_worry_value);
+                new_worry_items.push((monkey.FalseCase, new_worry_value))
             }
+            let cur_monkey = monkeys.get_mut(&monkey.ID).unwrap();
+            cur_monkey.ItemWorries.retain(|&x| x != worry_item);
         }
-        moves_map.insert(*id, moves);
     }
-    println!("{:?}", moves_map);
-    for (id, moves) in moves_map {
-        if let Some(monkey) = monkeys.get_mut(&id) {
-            let to_remove: Vec<u32> = moves.iter().map(|x| x.0).collect();
-            let mut current_worries = monkey.ItemWorries.clone();
-            println!("[{}]{:?}, to_remove: {:?}", id, current_worries, to_remove);
-            current_worries.retain(|x| !to_remove.contains(x));
-            println!("[{}]{:?}", id, current_worries);
-        };
-        
+    println!("Round {}", round);
+    for monkey in monkeys{
+        println!("{:?}", monkey);
     }
 }
