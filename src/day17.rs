4cdc744f4fe80fde6170bd61706b287a04fc6895
Prasanna Gautam <prasannagautam@gmail.com>
2022-12-17 13:31:44 -0800

    part two day17

--- src/day17.rs
+++ src/day17.rs
@@ -67,9 +67,28 @@ mod test_super {
     }
 }
 
-pub fn run() {
+fn run_sample() {
     let input = r#">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"#;
-    // let input = fs::read_to_string("./inputs/day-17").expect("Unable to read file");
+    part_two_solve(input);
+}
+
+fn part_one() {
+    let input = fs::read_to_string("./inputs/day-17").expect("Unable to read file");
+    part_one_solve(input.as_str());
+}
+
+fn part_two() {
+    let input = fs::read_to_string("./inputs/day-17").expect("Unable to read file");
+    part_two_solve(input.as_str());
+}
+
+pub fn run() {
+    // run_sample();
+    // part_one();
+    part_two();
+}
+
+fn part_one_solve(input: &str) {
     println!("input len={}", input.len());
     let symbols: [Symbol; 5] = [
         Symbol::from(vec![vec!['#', '#', '#', '#']]),
@@ -91,26 +110,15 @@ pub fn run() {
         Symbol::from(vec![vec!['#'], vec!['#'], vec!['#'], vec!['#']]),
         Symbol::from(vec![vec!['#', '#'], vec!['#', '#']]),
     ];
-
-    // let symbols: [Symbol; 5] = [
-    //     Symbol::from_rock_map(vec![(0, 0), (0, 1), (0, 2), (0, 3)]),
-    //     Symbol::from_rock_map(vec![(0, 1), (1, 0), (1, 2), (2, 1)]),
-    //     Symbol::from_rock_map(vec![(0, 0), (0, 1), (0, 2), (1, 2), (2, 2)]),
-    //     Symbol::from_rock_map(vec![(0, 0), (1, 0), (2, 0), (3, 0)]),
-    //     Symbol::from_rock_map(vec![(0, 0), (0, 1), (1, 0), (1, 1)]),
-    // ];
-
     for symbol in &symbols {
         println!("symbol.rock_map = {:?}", symbol.rock_map);
     }
-
     // allocate length of the current
     // make tall enough chamber to fit full run
     let mut chamber_map = [[0u8; 7]; 10000];
-
     let (mut i, mut tick, mut total_height) = (0, 0, 0);
     // let mut cache = HashMap::new();
-    while i < 2023 {
+    while i < 2022 {
         let symbol = &symbols[i % symbols.len()];
         let (mut h, mut w) = (get_height(&chamber_map) + 3, 2);
         loop {
@@ -144,25 +152,117 @@ pub fn run() {
             chamber_map[h + dh][w + dw] = b'#';
         }
 
-        // for h in chamber_map.iter().rev() {
-        //     if h == &[0u8; 7] {
-        //         continue;
-        //     }
-        //     for ch in h {
-        //         if *ch == 0 {
-        //             print!(".")
-        //         } else {
-        //             print!("{}", *ch as char);
-        //         }
-        //     }
-        //     println!();
-        // }
+        // print_chamber_map(chamber_map);
 
-        println!("height = {}", get_height(&chamber_map));
+        println!("rocks= {} height = {}", i + 1, get_height(&chamber_map));
+        println!("========================================");
+        i += 1;
+    }
+}
+
+fn part_two_solve(input: &str) {
+    println!("input len={}", input.len());
+    let symbols: [Symbol; 5] = [
+        Symbol::from(vec![vec!['#', '#', '#', '#']]),
+        Symbol::from(
+            /*
+            (0,1),(1,0),(1,1),(1,2),(2,1)
+            */
+            vec![
+                vec![' ', '#', ' '],
+                vec!['#', '#', '#'],
+                vec![' ', '#', ' '],
+            ],
+        ),
+        Symbol::from(vec![
+            vec!['#', '#', '#'],
+            vec![' ', ' ', '#'],
+            vec![' ', ' ', '#'],
+        ]),
+        Symbol::from(vec![vec!['#'], vec!['#'], vec!['#'], vec!['#']]),
+        Symbol::from(vec![vec!['#', '#'], vec!['#', '#']]),
+    ];
+    for symbol in &symbols {
+        println!("symbol.rock_map = {:?}", symbol.rock_map);
+    }
+    // allocate length of the current
+    // make tall enough chamber to fit full run
+    let mut chamber_map = [[0u8; 7]; 10000];
+    let (mut i, mut tick, mut total_height) = (0, 0, 0);
+    let mut cache = HashMap::new();
+    while i < 1000000000000 {
+        let symbol = &symbols[i % symbols.len()];
+        let (mut h, mut w) = (get_height(&chamber_map) + 3, 2);
+        loop {
+            // println!("h = {:?}", h);
+            // println!("w = {:?}", w);
+            match input.as_bytes()[tick % input.len()] {
+                b'>' => {
+                    if symbol.fits_in_chamber(&chamber_map, h, w + 1) {
+                        w += 1;
+                    }
+                }
+                b'<' => {
+                    if w != 0 {
+                        if symbol.fits_in_chamber(&chamber_map, h, w - 1) {
+                            w -= 1;
+                        }
+                    }
+                }
+                _ => panic!("invalid input"),
+            };
+            tick += 1;
+            if h == 0 || !symbol.fits_in_chamber(&chamber_map, h - 1, w) {
+                break;
+            }
+            h -= 1;
+        }
+        // println!("{:?}", symbol.rock_map.clone());
+        for (dh, dw) in symbol.rock_map.clone() {
+            // println!("dh, dw = {:?}, {:?}", dh, dw);
+            // println!("h, w = {:?}, {:?}", h + dh, w + dw);
+            chamber_map[h + dh][w + dw] = b'#';
+        }
+
+        // print_chamber_map(chamber_map);
+
+        println!(
+            "rocks= {} height = {}",
+            i + 1,
+            total_height + get_height(&chamber_map)
+        );
         println!("========================================");
 
-        // printed out value for 2020 which was correct at 3202
-        println!("i = {:?}", i);
+        let key = (
+            i % symbols.len(),
+            tick % input.len(),
+            chamber_heights(&chamber_map),
+        );
+        // idea is to find a point where we get a repetition -- if we find it, we can take that index and jump ahead
+        // dont need to iterate through rest
+        if let Some((idx, height)) = cache.get(&key) {
+            let repeats = (1000000000000 - idx) / (i - idx) - 1;
+            i += (i - idx) * repeats;
+            total_height += (get_height(&chamber_map) - height) * repeats;
+        } else {
+            cache.insert(key, (i, get_height(&chamber_map)));
+        }
         i += 1;
+    }
+}
+
+fn print_chamber_map(chamber_map: [[u8; 7]; 10000]) {
+    for h in chamber_map.iter().rev() {
+        if h == &[0u8; 7] {
+            continue;
+        }
+        for ch in h {
+            if *ch == 0 {
+                print!(".")
+            } else {
+                print!("{}", *ch as char);
+            }
+        }
+        println!();
     }
 }
