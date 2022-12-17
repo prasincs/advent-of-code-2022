3b5234fdc85f0fb764f0a71eddb0916b787d7d3c
Prasanna Gautam <prasannagautam@gmail.com>
2022-12-16 21:53:07 -0800

    day16 part one

--- src/day16.rs
+++ src/day16.rs
@@ -1,25 +1,42 @@
+use itertools::Itertools;
 use petgraph::algo::floyd_warshall;
 use petgraph::graph::Node;
 use petgraph::{prelude::*, Directed, Graph};
 use regex::Regex;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
+use std::{fs, vec};
 
 use lazy_static::lazy_static;
 
 lazy_static! {
     pub static ref RE: regex::Regex =
-        Regex::new(r"Valve (\S+) has flow rate=(\d+); tunnel(?:s?) lead(?:s?) to valve(?:s?) (.+)")
-            .unwrap();
+        Regex::new(r"Valve (\S+) has flow rate=(\d+); tunnels? leads? to valves? (.+)").unwrap();
 }
 
 #[derive(Debug, Default, Clone)]
 struct ValveInfo {
-    name: String,
+    index: usize,
     flow_rate: u32,
     tunnels: Vec<String>,
 }
 
+impl ValveInfo {
+    fn index_from(s: &str) -> usize {
+        let chars = s.as_bytes();
+        (chars[0] - b'A') as usize * 26 + (chars[1] - b'A') as usize
+    }
+}
+
+fn part_one() {
+    let input = fs::read_to_string("./inputs/day-16").expect("Unable to read file");
+    solve(input.as_str());
+}
+
 pub fn run() {
+    part_one();
+}
+
+fn run_sample() {
     /*
     DD -> CC=2, AA=0, EE=3
     CC */
@@ -35,53 +52,125 @@ Valve GG has flow rate=0; tunnels lead to valves FF, HH
 Valve HH has flow rate=22; tunnel leads to valve GG
 Valve II has flow rate=0; tunnels lead to valves AA, JJ
 Valve JJ has flow rate=21; tunnel leads to valve II"#;
+    solve(input);
+}
+
+fn solve(input: &str) {
     let valve_infos = parse_valve_infos(input);
-    let mut graph: Graph<(), (), Directed> = Graph::new();
-    let mut edges: Vec<(NodeIndex, NodeIndex)> = vec![];
-    let mut idx_map: HashMap<String, NodeIndex> = HashMap::new();
-    for valve_info in &valve_infos {
-        println!("{:?}", valve_info);
-        let idx = graph.add_node(());
-        idx_map.insert(valve_info.name.clone(), idx);
+    //floyd_warshall_library(valve_infos);
+    let distance_grid: Vec<Vec<u32>> = create_distance_grid(&valve_infos);
+    // pick valves with flow rate > 0 to start with
+    let closed_valves: Vec<&ValveInfo> = valve_infos
+        .iter()
+        .filter(|valve| valve.flow_rate > 0)
+        .collect();
+    let start = ValveInfo::index_from("AA");
+    let total_pressure = best_path(&distance_grid, closed_valves, start, 30);
+    println!("total_pressure={}", total_pressure);
+}
+
+fn best_path(
+    distances: &Vec<Vec<u32>>,
+    closed_valves: Vec<&ValveInfo>,
+    at_valve: usize,
+    minutes_left: u32,
+) -> u32 {
+    let mut pressures: Vec<u32> = vec![];
+
+    for valve in &closed_valves {
+        let distance = distances[at_valve][valve.index];
+
+        if distance >= minutes_left {
+            continue;
+        }
+
+        let pressure = valve.flow_rate * (minutes_left - distance - 1);
+
+        let remaining = closed_valves
+            .iter()
+            .filter(|v| v.index != valve.index)
+            .cloned()
+            .collect();
+
+        let best_pressure = pressure
+            + best_path(
+                distances,
+                remaining,
+                valve.index,
+                minutes_left - distance - 1,
+            );
+
+        pressures.push(best_pressure);
     }
 
-    for valve_info in &valve_infos {
-        for lead in valve_info.tunnels.clone() {
-            let connecting_idx = *idx_map.get(&lead).unwrap();
-            edges.push((
-                *idx_map.get(&valve_info.name.to_string()).unwrap(),
-                connecting_idx,
-            ));
+    *pressures.iter().max().unwrap_or(&0)
+}
+
+fn create_distance_grid(valve_infos: &Vec<ValveInfo>) -> Vec<Vec<u32>> {
+    let last_index = ValveInfo::index_from("ZZ");
+    let mut edges: Vec<Vec<usize>> = vec![vec![]; last_index + 1];
+    let vertices: Vec<usize> = valve_infos.iter().map(|valve| valve.index).collect();
+
+    for valve_info in valve_infos {
+        for tunnel in &valve_info.tunnels {
+            let tun_str = tunnel.as_str();
+            let tun_index = ValveInfo::index_from(tun_str);
+            edges[valve_info.index].push(tun_index);
         }
     }
-    graph.extend_with_edges(edges.clone());
-    let inf = std::i32::MAX;
+    let mut grid: Vec<Vec<u32>> = vec![vec![0; last_index + 1]; last_index + 1];
 
-    let res = floyd_warshall(&graph, |edge| -> i32 {
-        if edges.contains(&(edge.source(), edge.target())) {
-            1
-        } else {
-            inf
-        }
-    })
-    .unwrap();
-
-    for x in res {
-        println!("{:?}", x);
+    for valve_info in valve_infos {
+        let distance_vec = dijkstra(&edges, &vertices, last_index, valve_info.index);
+        grid[valve_info.index] = distance_vec;
     }
+
+    grid
+}
+
+fn dijkstra(edges: &[Vec<usize>], vertices: &[usize], last_index: usize, start: usize) -> Vec<u32> {
+    let mut distance_to: Vec<u32> = vec![u32::MAX - 1; last_index + 1];
+    let mut queue: HashSet<usize> = vertices.iter().cloned().collect();
+
+    // distance to itself is 0
+    distance_to[start] = 0;
+    while !queue.is_empty() {
+        let shortest_dist_idx = *queue
+            .iter()
+            .min_by(|&&a, &&b| distance_to[a].cmp(&distance_to[b]))
+            .unwrap();
+
+        queue.remove(&shortest_dist_idx);
+
+        let neighbours: Vec<usize> = edges[shortest_dist_idx]
+            .iter()
+            .filter(|valve| queue.contains(valve))
+            .cloned()
+            .collect();
+
+        for neighbor in neighbours {
+            let alt = distance_to[shortest_dist_idx] + 1;
+
+            if alt < distance_to[neighbor] {
+                distance_to[neighbor] = alt;
+            }
+        }
+    }
+    distance_to
 }
 
 fn parse_valve_infos(input: &str) -> Vec<ValveInfo> {
     let caps: Vec<ValveInfo> = input
         .split("\n")
         .map(|line| {
+            println!("{}", line);
             let caps = RE.captures(line).unwrap();
             let leading_to_valves: Vec<String> =
                 caps[3].split(",").map(|x| x.trim().to_string()).collect();
-            let name = caps[1].to_string();
             let flow_rate: u32 = caps[2].parse().unwrap();
+            let index = ValveInfo::index_from(&caps[1]);
             ValveInfo {
-                name,
+                index,
                 flow_rate,
                 tunnels: leading_to_valves,
             }
